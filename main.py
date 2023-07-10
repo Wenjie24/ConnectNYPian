011ccb712e5cbfae57f8874d1138b5e0c4474750
Wenjie24 <122337267+Wenjie24@users.noreply.github.com>
2023-07-11 00:53:42 +0800

    Added MYSQL cheat code
    hasan pls take note

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
-import MySQLdb.cursors
-from flask import Flask, render_template, request, redirect, url_for, session, app
+from flask import Flask, render_template, request, redirect, url_for, session
 import mysql.connector
 from flask_mysqldb import MySQL
+import MySQLdb.cursors
 from mysql.connector import Error
 from datetime import date, timedelta
 from forms import *
 
 app = Flask(__name__)
-app.permanent_session_lifetime = timedelta(minutes=5)
 
 # Config the Setting
-app.config['MYSQL_HOST'] = 'localhost',
-app.confg['MYSQL_USER'] = 'root',
-app.config['MYSQL_PASSWORD'] = 'somepassword' or 'wenjie',
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'wenjie'
 app.config['MYSQL_DB'] = 'connectnypian_db'  # Standardised schema name
 app.config['MYSQL_PORT'] = 3306
 
@@ -20,22 +19,57 @@ app.config['MYSQL_PORT'] = 3306
 mysql = MySQL(app)
 
 #Common MYSQL code
-# cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor) # To activate a cursor
-# cursor.execute('SELECT * FROM %s', (table)) # Execute a query
-# account = cursor.fetchone #Fetch one record
+# mycursor.execute('SELECT * FROM %s', (table)) # Execute a query
+# account = mycursor.fetchone #Fetch one record
 
 
-# EXTERNAL FUNCTIONS
+# MYSQL CHEAT CODE - Created by ur handsome daddy
+def connection_cursor(): #This function allow you to obtan the cursor
+    return mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+def cursor_close(cursor): #This function close the cursor that you obtained
+    cursor.close()
+def connection_commit(): #This function commit the query
+    mysql.connection.commit()
+def connection_close(): #This function close the connection
+    mysql.connection.close()
+
+def execute_fetchone(query): #Get cursor, execute and close
+    try:
+        cursor = connection_cursor()
+        cursor.execute(query)
+        result = cursor.fetchone()
+        cursor_close(cursor)
+        return result
+    except Error as e:
+        print("Error Executing query:", e)
+        return None
+
+def execute_fetchall(query): #Get cursor, execute and close
+    try:
+        cursor = connection_cursor()
+        cursor.execute(query)
+        result = cursor.fetchall()
+        cursor_close(cursor)
+        return result
+    except Error as e:
+        print("Error Executing query:", e)
+        return None
+
+# END OF CHEAT CODE
+
 
 # function for creating a comment, must assign createcomment form to a variable in applicable routes
 def createcomment(form):
     if request.method == 'POST':
         body = form.body.data
 
+
         sql = "INSERT INTO comments (body) VALUES (%s)"
         val = (body)
+        mycursor = obtain_cursor() # To activate a cursor
         mycursor.execute(sql, val)
-        mydb.commit()
+        mysql.connection.commit()
+        close_cursor(mycursor)
         print("comment added to database")
 
     pass
@@ -46,6 +80,7 @@ def createlike(post_id):
         like_date = date.today
         sql = "INSERT INTO like (like_date, post_id, account_id) VALUES (%s, %s, %s)"
         val = (like_date, post_id, session[id])
+        mycursor = obtain_cursor()
         mycursor.execute(sql, val)
         mydb.commit()
 
@@ -103,5 +138,6 @@ def createpost():
     return render_template('/processes/createpost.html', form=form)
 
 
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
