06dc94849d17d7e0f8aff60aabbd79f659dddc25
Wenjie24 <122337267+Wenjie24@users.noreply.github.com>
2023-07-17 00:46:35 +0800

    validator regexp

--- forms.py
+++ forms.py
@@ -10,12 +10,12 @@ class create_comment(Form):
     body = TextAreaField('', render_kw={'placeholder': 'Add a comment:'})
 
 class signup_form(Form):
-    username = StringField('Username', validators=[validators.regexp('^[A-za-z1-9]+$'), validators.DataRequired()]) # Regex to allow only string
-    email = StringField('School Email', validators=[validators.DataRequired()])
-    password = PasswordField('Password', validators=[validators.DataRequired(), validators.regexp('^[A-za-z1-9]+$'), validators.length(min=8, max=64)])
+    username = StringField('Username', validators=[validators.regexp('^[A-za-z1-9]+$', message='Username should not contain symbol.'), validators.DataRequired()]) # Regex to allow only string
+    email = StringField('School Email', validators=[validators.DataRequired(), validators.regexp('^[a-zA-Z0-9]+@mymail\.nyp\.edu\.sg$', message='Must be a valid xxxxxxx@mymail.nyp.edu.sg')])
+    password = PasswordField('Password', validators=[validators.DataRequired(), validators.regexp('^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[\W_]).{8,64}$', message='Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character. It should be between 8 and 64 characters in length.')])
     recaptcha = RecaptchaField()
 
 
 class login_form(Form):
-    username = StringField('Username', validators=[validators.regexp('^[A-za-z]+$'), validators.DataRequired()]) # Regex to allow only string
-    password = PasswordField('Password', validators=[validators.DataRequired(), validators.regexp('^[A-za-z1-9]+$')])
+    username = StringField('Username', validators=[validators.regexp('^[A-za-z1-9]+$', message='Username should not contain symbol.'), validators.DataRequired()]) # Regex to allow only string
+    password = PasswordField('Password', validators=[validators.DataRequired()])
